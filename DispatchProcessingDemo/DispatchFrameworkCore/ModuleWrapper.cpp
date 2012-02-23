081aa3ed5e30934771355b51352561c02ddb3a1f
Christopher Jones <cdj@cmslpc10.fnal.gov>
2012-02-23 03:32:04 -0600

    Made m_requestedPrefetch volatile to improve cross thread reads. Gave the run and prefetch queues different names. Improved documentation.

--- DispatchProcessingDemo/DispatchFrameworkCore/ModuleWrapper.cpp
+++ DispatchProcessingDemo/DispatchFrameworkCore/ModuleWrapper.cpp
@@ -19,6 +19,7 @@
 using namespace demo;
 
 static const std::string kPrefix("gov.fnal.prefetch.");
+static const std::string kRunPrefix("gov.fnal.run.");
 
 static unsigned long nextID() {
   static unsigned long s_id =0;
@@ -38,7 +39,7 @@ m_module(iModule),
 m_event(iEvent),
 m_prefetchGroup(dispatch_group_create()),
 m_prefetchQueue(dispatch_queue_create(unique_name(kPrefix+iModule->label()).c_str(), NULL)),
-m_runQueue(dispatch_queue_create(unique_name(kPrefix+iModule->label()).c_str(), NULL)),
+m_runQueue(dispatch_queue_create(unique_name(kRunPrefix+iModule->label()).c_str(), NULL)),
 m_requestedPrefetch(false)
 {
 }
@@ -54,7 +55,7 @@ m_requestedPrefetch(false)
   if(m_module->threadType() == kThreadSafeBetweenInstances) {
     //the same instance can be called reentrantly so each Schedule can have
     // its own queue for each instance rather than having to share one queue
-    m_runQueue = dispatch_queue_create(unique_name(kPrefix+m_module->label()).c_str(), NULL);
+    m_runQueue = dispatch_queue_create(unique_name(kRunPrefix+m_module->label()).c_str(), NULL);
   } else {
     m_runQueue=iOther.m_runQueue;
     dispatch_retain(m_runQueue);
@@ -85,7 +86,7 @@ ModuleWrapper::operator=(const ModuleWrapper& iOther)
     dispatch_release(m_runQueue);
     m_runQueue=iOther.m_runQueue;
     dispatch_retain(m_runQueue);
-    m_requestedPrefetch = iOther.m_requestedPrefetch;    
+    m_requestedPrefetch = iOther.m_requestedPrefetch;  
   }
   return *this;
 }
@@ -106,15 +107,37 @@ ModuleWrapper::do_prefetch_task(void* iContext)
 void
 ModuleWrapper::doPrefetch()
 {
+  //This routine is only called from within a queue so its
+  // operations are atomic
   if (not m_requestedPrefetch) {
+    m_requestedPrefetch = true;
     module()->prefetchAsync(*m_event, m_prefetchGroup); 
-    m_requestedPrefetch=true;
   }  
 }
 
 void 
 ModuleWrapper::prefetchAsync()
 {
+  //NOTE: we must guarantee that we've entered the group before leaving
+  // this routine OR that the group has already been entered by another thread
+  // which we can tell because m_requestedPrefetch can only be set to true
+  // in the atomic operation which would have been done by the dispatch_group_async_f.
+  // Having multiple threads call ModuleWrapper::doPrefetch is OK since only the
+  // first one will actually do the prefetching. However it is not desirable since
+  // we still have to wait until all the outstanding tasks created by this dispatch_group_async_f
+  // have been run before the next step can be taken.
+  
+  //NOTE: If m_requestedPrefetch were atomic, we could do
+  // dispatch_group_enter(m_refetchGroup.get());
+  // if(module()->hasPrefetchItems() and ( not atomic_get_previous_and_set(m_requestedPrefetch,true)) {
+  //    module()->prefetchAsync(*m_event,m_prefetchGroup);
+  //  }
+  //  dispatch_group_leave(m_prefetchGroup.get());
+  // This would work because even if atomic_get_previous_and_set(m_requestedPrefetch,true) returned true
+  // we'd know that another thread must either still have an outstanding dispatch_group_enter and is
+  // running module()->prefetchAsync(...) or that a thread has already finished module()->prefetchAsync
+  // and all prefetchAsync has already acquired the dispatch_group_enter or all prefetches have finished
+  
   if(module()->hasPrefetchItems() and (not m_requestedPrefetch)) {
     dispatch_group_async_f(m_prefetchGroup.get(),m_prefetchQueue,
                            static_cast<void*>(this),
