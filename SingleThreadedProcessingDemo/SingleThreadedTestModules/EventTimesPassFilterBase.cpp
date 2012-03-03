85b2720f9ace13a2e6674448ba8be0995b906282
Chris Jones <cdj@CDJ.local>
2012-03-02 18:22:22 -0600

    new base class for EventTimes modules

---
+++ SingleThreadedProcessingDemo/SingleThreadedTestModules/EventTimesPassFilterBase.cpp
@@ -0,0 +1,45 @@
+//
+//  EventTimesPassFilterBase.cpp
+//  DispatchProcessingDemo
+//
+//  Created by Chris Jones on 10/13/11.
+//  Copyright 2011 FNAL. All rights reserved.
+//
+
+#include <iostream>
+#include <unistd.h>
+
+#include "EventTimesPassFilterBase.h"
+#include "Event.h"
+
+namespace demo {
+  
+  EventTimesPassFilterBase::EventTimesPassFilterBase(const boost::property_tree::ptree& iConfig):
+  ConfiguredFilter(iConfig){
+    
+    for(const boost::property_tree::ptree::value_type& v: iConfig.get_child("toGet")) {
+      m_getters.push_back(registerGet(v.second.get<std::string>("label"), 
+                                      ""));
+    }
+    m_eventTimes.reserve(iConfig.get_child("eventTimes").size());
+    for(const boost::property_tree::ptree::value_type& v: iConfig.get_child("eventTimes")) {
+      m_eventTimes.push_back(v.second.get_value<float>());
+    }
+  }
+  
+  bool 
+  EventTimesPassFilterBase::filter(const edm::Event& iEvent) {
+    //printf("Producer %s\n",label().c_str());
+    int sum=0;
+    for(std::vector<const Getter*>::iterator it = m_getters.begin(), itEnd=m_getters.end();
+        it != itEnd;
+        ++it) {
+      sum +=iEvent.get(*it);
+      //printf("%s got %s with value %i\n",label().c_str(), (*it)->label().c_str(), iEvent.get((*it)));
+    }
+    unsigned long index = iEvent.index() % m_eventTimes.size();
+    
+    wait(m_eventTimes[index]);
+    return true;
+  }
+}
