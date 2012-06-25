317fdf2c575bb1b871eb41cd939b8aeaafab6819
Chris Jones <cdj@cdj.dhcp.fnal.gov>
2012-06-25 13:38:27 -0500

    Removed comments left from libdispatch version. Simplified logic and removed manipulation of ref_count since we know the task is always created just before passing to this routine and we know Module::prefetchAsync will cause the task to run.

--- TBBProcessingDemo/TBBFrameworkCore/ModuleWrapper.cpp
+++ TBBProcessingDemo/TBBFrameworkCore/ModuleWrapper.cpp
@@ -77,37 +77,19 @@ ModuleWrapper::prefetchAsync(tbb::task* iPrefetchDoneTask)
   // first one will actually do the prefetching. However it is not desirable since
   // we still have to wait until all the outstanding tasks created by this dispatch_group_async_f
   // have been run before the next step can be taken.
+  assert(iPrefetchDoneTask->ref_count()==0);
   
-  //NOTE: If m_requestedPrefetch were atomic, we could do
-  // dispatch_group_enter(m_refetchGroup.get());
-  // if(module()->hasPrefetchItems() and ( not atomic_get_previous_and_set(m_requestedPrefetch,true)) {
-  //    module()->prefetchAsync(*m_event,m_prefetchGroup);
-  //  }
-  //  dispatch_group_leave(m_prefetchGroup.get());
-  // This would work because even if atomic_get_previous_and_set(m_requestedPrefetch,true) returned true
-  // we'd know that another thread must either still have an outstanding dispatch_group_enter and is
-  // running module()->prefetchAsync(...) or that a thread has already finished module()->prefetchAsync
-  // and all prefetchAsync has already acquired the dispatch_group_enter or all prefetches have finished
-
-  //NOTE: do I need to increment and then decrement the iPrefetchDoneTask ref count?
-  //dispatch_group_enter(m_prefetchGroup.get());
-   iPrefetchDoneTask->increment_ref_count();
-  
-  if(module()->hasPrefetchItems() and (not m_requestedPrefetch.test_and_set())) {
-    module()->prefetchAsync(*m_event, iPrefetchDoneTask); 
-    if(0==iPrefetchDoneTask->decrement_ref_count()) {
-       //if everything finishes before we leave this routine, we need to launch the task
-       tbb::task::spawn(*iPrefetchDoneTask);
-    } 
-  } else if(module()->hasPrefetchItems()) {
-     //we already have one prefetch task running and the done task is identical 
-     // so it can be gotten rid of
-     tbb::task::destroy(*iPrefetchDoneTask);
+  if(module()->hasPrefetchItems()) {
+    
+    if (not m_requestedPrefetch.test_and_set()) {
+      module()->prefetchAsync(*m_event, iPrefetchDoneTask); 
+    } else {
+      //we already have one prefetch task running and the done task is identical 
+      // so it can be gotten rid of
+      tbb::task::destroy(*iPrefetchDoneTask);
+    }
   } else {
-     if(0==iPrefetchDoneTask->decrement_ref_count()) {
-        //if everything finishes before we leave this routine, we need to launch the task
-        tbb::task::spawn(*iPrefetchDoneTask);
-     } 
+    //if everything finishes before we leave this routine, we need to launch the task
+    tbb::task::spawn(*iPrefetchDoneTask);
   }
-  //dispatch_group_leave(m_prefetchGroup.get());
 }
