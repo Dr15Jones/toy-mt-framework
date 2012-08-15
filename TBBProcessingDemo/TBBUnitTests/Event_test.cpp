5d418cc432cd71e4b8ccec7b0a871e8e2d82250e
Chris Jones <cdj@cdj.dhcp.fnal.gov>
2012-08-15 13:55:21 -0500

    Test now uses TBB rather than libdispatch (from where it was copied)

--- TBBProcessingDemo/TBBUnitTests/Event_test.cpp
+++ TBBProcessingDemo/TBBUnitTests/Event_test.cpp
@@ -11,9 +11,12 @@
 #include <cppunit/extensions/HelperMacros.h>
 #include <unistd.h>
 #include <memory>
+#include <atomic>
 #include "Event.h"
 #include "Producer.h"
-#include <dispatch/dispatch.h>
+#include "SerialTaskQueue.h"
+#include "tbb/task.h"
+#include "tbb/parallel_for.h"
 
 class Event_test : public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE(Event_test);
@@ -108,47 +111,21 @@ namespace  {
   class ThreadSafeCounter {
   public:
     ThreadSafeCounter(const char* iName):
-    m_count(0),
-    m_queue(dispatch_queue_create(iName,NULL)) {
-      //dispatch_retain(m_queue);
-    }
+    m_count(0){}
     
     int increment() {
-      int v = 0;
-      Context c(m_count,v);
-      dispatch_sync_f(m_queue, &c, ThreadSafeCounter::increment_task);
-      return v;
+       return ++m_count;
     }
     
     int value() {
-      int v=0;
-      Context c(m_count,v);
-      dispatch_sync_f(m_queue,&c,ThreadSafeCounter::value_task);
-      return v;
+       return m_count;
     }
     
     ~ThreadSafeCounter() {
-      dispatch_release(m_queue);
     }
   private:
     
-    struct Context {
-      Context(int& iCount, int& iValue):
-      count(&iCount), value(&iValue) {}
-      int* count;
-      int* value;
-    };
-    static void increment_task(void* iContext) {
-      Context* c = reinterpret_cast<Context*>(iContext);
-      *(c->value) = ++(*c->count);
-    };
-    static void value_task(void* iContext) {
-      Context* c = reinterpret_cast<Context*>(iContext);
-      *(c->value) = (*c->count);
-    };
-    
-    int m_count;
-    dispatch_queue_t m_queue;
+    std::atomic<int> m_count;
   };
 
   class AccessTestingProd : public demo::Producer {
@@ -358,13 +335,14 @@ void Event_test::simultaneousBetweenInstancesTwoEvents()
   
   event1.addProducer(at);
   
-  std::auto_ptr<demo::Event> event2(event1.clone());
+  std::unique_ptr<demo::Event> event2(event1.clone());
   event2->setIndex(2);
 
   const demo::Event* events[2]={&event1,event2.get()};
-  const demo::Event** pEvents=events;
-  
-  dispatch_apply_f(2, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, NULL), pEvents,get_task);
+
+  tbb::parallel_for(0,2,[events](int iIndex) {
+     events[iIndex]->get("access","");
+  });
   CPPUNIT_ASSERT(count.value()==2);
   CPPUNIT_ASSERT(wasChanged==true);
 }
@@ -380,13 +358,15 @@ void Event_test::simultaneousBetweenModulesTwoEvents()
   event1.setIndex(1);
   
   event1.addProducer(at);
-  std::auto_ptr<demo::Event> event2(event1.clone());
+  std::unique_ptr<demo::Event> event2(event1.clone());
   event2->setIndex(2);
 
   const demo::Event* events[2]={&event1,event2.get()};
-  const demo::Event** pEvents=events;
   
-  dispatch_apply_f(2, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, NULL), pEvents,get_task);
+  tbb::parallel_for(0,2,[events](int iIndex) {
+     events[iIndex]->get("access","");
+  });
+  //dispatch_apply_f(2, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, NULL), pEvents,get_task);
   CPPUNIT_ASSERT(count.value()==2);
   CPPUNIT_ASSERT(wasChanged==false);
 }
@@ -402,13 +382,15 @@ void Event_test::simultaneousThreadUnsafeTwoEvents()
   event1.setIndex(3);
   
   event1.addProducer(at);
-  std::auto_ptr<demo::Event> event2(event1.clone());
+  std::unique_ptr<demo::Event> event2(event1.clone());
   event2->setIndex(4);
 
   const demo::Event* events[2]={&event1,event2.get()};
-  const demo::Event** pEvents=events;
   
-  dispatch_apply_f(2, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, NULL), const_cast<demo::Event**>(pEvents),get_task);
+  tbb::parallel_for(0,2,[events](int iIndex) {
+     events[iIndex]->get("access","");
+  });
+  //dispatch_apply_f(2, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, NULL), const_cast<demo::Event**>(pEvents),get_task);
   CPPUNIT_ASSERT(count.value()==2);
   CPPUNIT_ASSERT(wasChanged==false);
 }
