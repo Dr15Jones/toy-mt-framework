3973a6f00a7bafc9129b597e9b407f2c6c8cca37
Chris Jones <cdj@cdj.dhcp.fnal.gov>
2012-08-16 13:27:16 -0500

    Fixed design flaw. Tasks are shared across WaitingTaskLists and therefore the WaitingTaskLists cannot modify those task. Switched to using WaitNode instead.

--- TBBProcessingDemo/TBBFrameworkCore/WaitingTaskList.cpp
+++ TBBProcessingDemo/TBBFrameworkCore/WaitingTaskList.cpp
@@ -7,29 +7,70 @@
  *
  */
 
+#include "tbb/task.h"
 #include "WaitingTaskList.h"
-#include "WaitableTask.h"
+#include <iostream>
+#include <cassert>
 
 using namespace demo;
 
 
 void
-WaitingTaskList::add(WaitableTask* iTask) {
+WaitingTaskList::reset()
+{
+   m_waiting = true;
+   unsigned int nSeenTasks = m_lastAssignedCacheIndex;
+   m_lastAssignedCacheIndex = 0;
+   assert(m_head == 0);
+   if (nSeenTasks > m_nodeCacheSize) {
+      //need to expand so next time we don't have to do any
+      // memory requests
+      delete [] m_nodeCache;
+      m_nodeCacheSize = nSeenTasks;
+      m_nodeCache = new WaitNode[nSeenTasks];
+      for(auto it = m_nodeCache, itEnd = m_nodeCache+m_nodeCacheSize; it!=itEnd; ++it) {
+         it->m_fromCache=true;
+      }
+   }
+}
+
+WaitNode* 
+WaitingTaskList::createNode(tbb::task* iTask)
+{
+   unsigned int index = m_lastAssignedCacheIndex++;
+   
+   WaitNode* returnValue;
+   if( index < m_nodeCacheSize) {
+      returnValue = m_nodeCache+index;
+   } else {
+      returnValue = new WaitNode;
+      returnValue->m_fromCache=false;
+   }
+   returnValue->m_task = iTask;
+   returnValue->m_next = returnValue;
+   
+   return returnValue;
+}
+
+
+void
+WaitingTaskList::add(tbb::task* iTask) {
    iTask->increment_ref_count();
    if(!m_waiting) {
       if(0==iTask->decrement_ref_count()) {
          tbb::task::spawn(*iTask);
       }
    } else {
-      WaitableTask* oldHead = m_head.exchange(iTask);
+      WaitNode* newHead = createNode(iTask);
+      WaitNode* oldHead = m_head.exchange(newHead);
       if(oldHead) {
-        iTask->setNextTask(oldHead);
+        newHead->setNextNode(oldHead);
         //NOTE: even if 'm_waiting' changed, we don't
         // have to recheck since we beat 'announce()' in
         // the ordering of 'm_head.exchange' call so iTask
         // is guaranteed to be in the link list
       } else {
-        iTask->setNextTask(0);
+        newHead->setNextNode(0);
         if(!m_waiting) {
            //if finished waiting right before we did the
            // exchange our task will not be spawned. Also,
@@ -58,23 +99,24 @@ WaitingTaskList::announce()
    //Need a temporary storage since one of these tasks could
    // cause the next event to start processing which would refill 
    // this waiting list after it has been reset
-   WaitableTask* t = m_head.exchange(0);
-   WaitableTask* next;
-   while(t) {
+   WaitNode* n = m_head.exchange(0);
+   WaitNode* next;
+   while(n) {
       //it is possible that 'WaitingTaskList::add' is running in a different
       // thread and we have a new 'head' but the old head has not yet been
-      // attached to the new head (we identify this since 'nextTask' will return itself).
+      // attached to the new head (we identify this since 'nextNode' will return itself).
       //  In that case we have to wait until the link has been established before going on.
-      // We also have to get 'next' before spawning t since spawning t will
-      // eventually cause the task to be deleted and we don't want a race 
-      // condition between delete and calling 'nextTask()'.
-      while(t == (next=t->nextTask())) {
+      while(n == (next=n->nextNode())) {
          hardware_pause();
       }
+      auto t = n->m_task;
       if(0==t->decrement_ref_count()){
          tbb::task::spawn(*t);
       }
-      t=next;
+      if(!n->m_fromCache ) {
+         delete n;
+      }
+      n=next;
    }
 }
 
--- TBBProcessingDemo/TBBFrameworkCore/WaitingTaskList.h
+++ TBBProcessingDemo/TBBFrameworkCore/WaitingTaskList.h
@@ -10,26 +10,58 @@
 #define TBBProcessingDemo_WaitingTaskList_h
 
 #include <atomic>
+namespace tbb {
+   class task;
+}
 
 namespace demo {
-   class WaitableTask;
+   struct WaitNode {
+      tbb::task* m_task;
+      std::atomic<WaitNode*> m_next;
+      bool m_fromCache;
+      
+      void setNextNode(WaitNode* iNext){
+         m_next = iNext;
+      }
+      
+      WaitNode* nextNode() const {
+         return m_next;
+      }
+   };
+   
    class WaitingTaskList {
    public:
-      WaitingTaskList():
-      m_waiting{true},
-      m_head{0}
-      {  }
+      WaitingTaskList(unsigned int iInitialSize = 2):
+      m_head{0},
+      m_nodeCache{new WaitNode[iInitialSize]},
+      m_nodeCacheSize{iInitialSize},
+      m_lastAssignedCacheIndex{0},
+      m_waiting{true}
+      {
+         for(auto it = m_nodeCache, itEnd = m_nodeCache+m_nodeCacheSize; it!=itEnd; ++it) {
+            it->m_fromCache=true;
+         }
+      }
 
-      void add(WaitableTask*);
+      ~WaitingTaskList() {
+         delete [] m_nodeCache;
+      }
+      void add(tbb::task*);
 
       void doneWaiting();
-      void reset() { m_waiting = true;}
+      
+      ///NOTE: reset is not thread safe
+      void reset();
   
    private:
       //safe to call from multiple threads
       void announce();
 
-      std::atomic<WaitableTask*> m_head;
+      WaitNode* createNode(tbb::task* iTask);
+      std::atomic<WaitNode*> m_head;
+      WaitNode* m_nodeCache;
+      unsigned int m_nodeCacheSize;
+      std::atomic<unsigned int> m_lastAssignedCacheIndex;
       std::atomic<bool> m_waiting;
    };
 }
