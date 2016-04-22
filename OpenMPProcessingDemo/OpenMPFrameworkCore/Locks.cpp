7e12503f417e9fd839ca7ddee5b24a5432841e13
Christopher Jones <chrisdjones15@gmail.com>
2016-04-22 15:54:59 -0500

    Removed directories not needed for GeantV demonstrator

--- OpenMPProcessingDemo/OpenMPFrameworkCore/Locks.cpp
+++
@@ -1,7 +0,0 @@
-#include "Locks.h"
-
-#if defined(PARALLEL_MODULES)
-boost::shared_ptr<demo::OMPLock> demo::s_thread_unsafe_lock{ new OMPLock{}};
-#else
-boost::shared_ptr<demo::OMPLock> demo::s_thread_unsafe_lock{ new OMPLock{}};
-#endif
