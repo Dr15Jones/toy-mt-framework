bdebb6596a24d21498b9659909574c4a7cdb89b7
Christopher Jones <chrisdjones15@gmail.com>
2018-10-08 16:36:49 -0500

    Added EventTimesBusyWaitParallelForPassFilter
    This is the busy wait equivalent of the newly introduced
    EventTimesSleepParallelForPassFilter

---
+++ CommonTestModules/EventTimesBusyWaitParallelForPassFilter.cpp
@@ -0,0 +1,45 @@
+//
+//  EventTimesBusyWaitParallelForPassFilter.cpp
+//  DispatchProcessingDemo
+//
+//  Created by Chris Jones on 10/12/11.
+//  Copyright 2011 FNAL. All rights reserved.
+//
+
+#include <iostream>
+#include <unistd.h>
+#include <cassert>
+#include <atomic>
+#include "busy_wait_scale_factor.h"
+#include "busyWait.h"
+
+#include <vector>
+#include "EventTimesPassFilterBase.h"
+#include "parallel_for.h"
+
+namespace demo {
+  class EventTimesBusyWaitParallelForPassFilter : public EventTimesPassFilterBase {
+  public:
+    explicit EventTimesBusyWaitParallelForPassFilter(const boost::property_tree::ptree& iConfig);
+  private:
+    void wait(double iSeconds) override;
+    const unsigned int m_nRanges;
+  };
+  
+  EventTimesBusyWaitParallelForPassFilter::EventTimesBusyWaitParallelForPassFilter(const boost::property_tree::ptree& iConfig):
+    EventTimesPassFilterBase(iConfig),
+    m_nRanges{iConfig.get<unsigned int>("nRanges")}
+  {
+  }
+  
+  void
+  EventTimesBusyWaitParallelForPassFilter::wait(double iSeconds){
+    std::atomic<unsigned int> nRangesDone{0};
+    parallel_for(m_nRanges,[this,iSeconds,&nRangesDone](int) {
+        busyWait((iSeconds*busy_wait_scale_factor)/m_nRanges);
+        ++nRangesDone;
+      });
+    assert(nRangesDone == m_nRanges);
+  }
+}
+REGISTER_FILTER(demo::EventTimesBusyWaitParallelForPassFilter);
