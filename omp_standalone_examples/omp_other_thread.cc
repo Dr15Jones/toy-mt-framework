c1066ceb7a22faa7c2d53c4398b052cc10272093
Christopher Jones <chrisdjones15@gmail.com>
2018-10-11 14:34:05 -0500

    Simplistic try at starting task from a non-OpenMP thread
    This runs the task on the other thread instead of scheduling it
    on the threads already owned by OpenMP.

---
+++ omp_standalone_examples/omp_other_thread.cc
@@ -0,0 +1,91 @@
+//g++ -std=c++17 -O3 omp_other_thread.cc -fopenmp -o omp_other_thread
+//clang++ -std=c++17 -O3 omp_other_thread.cc -fopenmp -o omp_other_thread
+#include "omp.h"
+#include <cmath>
+#include <atomic>
+#include <thread>
+#include <mutex>
+#include <iostream>
+
+namespace {
+  std::atomic<unsigned int> threadIDCount{0};
+  
+  std::mutex s_coutMutex;
+
+  unsigned int threadID() {
+    static thread_local const unsigned int s_id{ ++threadIDCount };
+    return s_id;
+  }
+
+  template<typename T>
+  void safeCout( T&& iF) {
+    std::unique_lock<std::mutex> l(s_coutMutex);
+    iF(std::cout);
+  }
+
+  double integrate(int nIterations) {
+    double v = 0;
+    for(int k=0; k<nIterations; ++k) {
+      v += std::sin(3.14159/2.*k/nIterations)/double(nIterations);
+    }
+    return v;
+  }
+
+}
+
+constexpr int nIterations = 1000000;
+
+int main() {
+
+  omp_set_num_threads(2);
+
+  std::atomic<bool> startServer{false};
+  std::atomic<int> nCalls{0};
+
+  std::thread serverThread([&startServer,&nCalls]() {
+      safeCout([](std::ostream& out) {
+          out << "server thread id "<<threadID()<<std::endl;
+        });
+      while(not startServer.load()) ;
+
+#pragma omp task untied default(shared)
+      {
+        safeCout([](auto& out) {
+            out << "begin task thread id "<<threadID()<<std::endl;
+          });
+        if( 0.5 < integrate(nIterations)) {
+          ++nCalls;
+        }
+        safeCout([](auto& out) {
+            out << "end task thread id "<<threadID()<<std::endl;
+          });
+      }
+    });
+
+  std::atomic<int> nStartedTasksFromLoop{0};
+#pragma omp parallel
+  {
+#pragma omp single
+#pragma omp taskloop
+    for(int i =0; i<3; ++i) 
+    {
+      safeCout([i](auto& out) {
+          out << "begin taskloop index "<<i<<" thread id "<<threadID()<<std::endl;
+        });
+      if(++nStartedTasksFromLoop ==2) {
+        startServer=true;
+      }
+      if( 0.5 < integrate(nIterations)) {
+        ++nCalls;
+      }
+      safeCout([i](auto& out) {
+          out << "end taskloop index "<<i<<" thread id "<<threadID()<<std::endl;
+        });
+    }
+  }
+
+  serverThread.join();
+  std::cout <<"nCalls "<<nCalls.load()<<std::endl;
+
+  return 0;
+}
