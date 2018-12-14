c8dd2ab107c77919362d65a166713cd359b5b8d5
Christopher Jones <chrisdjones15@gmail.com>
2018-12-14 15:22:24 -0600

    Minor code improvements
    -Use DEBUG_OUT option to turn on/off output during process
    -Added more comments
    -Removed unused code and unneeded comments
    -Keep a count of how many times task stealing occured

--- omp_standalone_examples/omp_bad_task_steal.cc
+++ omp_standalone_examples/omp_bad_task_steal.cc
@@ -4,6 +4,9 @@
 //
 // Using clang, we do see task stealing.
 //clang++ -std=c++17 -O3 omp_bad_task_steal.cc -fopenmp -o omp_bad_task_steal
+//
+// To run
+//./omp_bad_task_steal 20 10 500 
 #include "omp.h"
 #include <cmath>
 #include <atomic>
@@ -13,7 +16,11 @@
 #include <unistd.h>
 //#include <sys/time.h>
 
+//#define DEBUG_OUT
+
 namespace demo {
+
+  //A general interface usable by both TBB and OpenMP implementations
   template<typename T>
     void parallel_for(unsigned int iRanges, T iFunctor) {
 #pragma omp taskloop
@@ -24,25 +31,26 @@ namespace demo {
 }
 
 namespace {
-  std::atomic<unsigned int> threadIDCount{0};
   
   std::mutex s_coutMutex;
 
+  std::atomic<unsigned int> threadIDCount{0};
   unsigned int threadID() {
     static thread_local const unsigned int s_id{ ++threadIDCount };
     return s_id;
   }
 
-  double integrate(long int nIterations) {
-    double v = 0.;
-    for(long int k=0; k<nIterations; ++k) {
-      v += std::sin(3.14159/2.*k/nIterations)/double(nIterations);
-    }
-    return v;
-  }
-
+  /*
+    We want to keep track of how much concurrency the program is accomplishing
+    and the case where we have task stealing occuring.
+   */
   std::atomic<int> countRunningTasks{0};
   std::atomic<int> maxRunningTasks{0};
+  std::atomic<int> nTimesMultipleTasksOnThread{0};
+
+  /* iState == true means we are starting a task on this thread
+     iState == false means we are stopping the task
+   */
   void task_on_thread_running(bool iState) {
 
     static thread_local unsigned int task_on_thread_count{0};
@@ -55,8 +63,11 @@ namespace {
 
       ++task_on_thread_count;
       if(task_on_thread_count > 1) {
+        ++nTimesMultipleTasksOnThread;
+#if defined(DEBUG_OUT)
         std::unique_lock<std::mutex> l{s_coutMutex};
         std::cout <<"more than one task ("<<task_on_thread_count<<") on thread "<<threadID()<<std::endl;
+#endif
       }
     } else {
       --task_on_thread_count;
@@ -65,11 +76,13 @@ namespace {
   }
 
 
+  // Keeps track of how processing is proceeding
   struct ProcessingState {
     std::atomic<int> m_leftToProcess;
     std::atomic<int> m_nProcessed;
   };
 
+  // In the full program, only one thread can do work on the storage at a time
   class MockStorage {
   public:
     MockStorage(long int nIterations) : m_nIterations{nIterations}, m_inUse{false},m_nWaiting{0} {}
@@ -79,8 +92,8 @@ namespace {
 
     void doNext(ProcessingState& iState);
     void store(ProcessingState& iState);
-    std::atomic<bool> m_inUse;
-    std::atomic<int> m_nWaiting;
+    std::atomic<bool> m_inUse;  //True if a thread is running storage task
+    std::atomic<int> m_nWaiting; //Number of processing loops that want storage to happen
     const long int m_nIterations;
   };
 
@@ -88,11 +101,14 @@ namespace {
 
   void processNextAsync(ProcessingState& iState, MockStorage& iStore, long int nIterations) {
     auto l = --iState.m_leftToProcess;
-    if( l > 0 ) {
+    if( l >= 0 ) {
       {
+#if defined(DEBUG_OUT)
         std::unique_lock<std::mutex> lck{s_coutMutex};
         std::cout <<l<<std::endl;
+#endif
       }
+      //Only once these 3 tasks complete can we run storage
       auto sp = std::make_shared<std::atomic<short>>(3);
 #pragma omp task untied default(shared) firstprivate(nIterations, sp)
       {
@@ -115,6 +131,7 @@ namespace {
     usleep(nIterations);
     task_on_thread_running(false);
     if ( 0 == --(*iPtr) ) {
+      //if this is the last task for this processing, queue up storage
       iStore.storeAsync(iState);
     }
   }
@@ -152,20 +169,23 @@ namespace {
 
     std::atomic<int> nParallel{0};
     std::atomic<int> maxParallel{0};
+
+    //There is some tuning going on to show the effect
+    // storage has to run long enough to task steal but shorter than all other tasks
+    // so the stolen task prolongs the storage time sufficiently to cause a problem.
+
     demo::parallel_for(5, [nIterations=m_nIterations, &nParallel, &maxParallel, tid](int iIndex) {
         auto p = ++nParallel;
         auto max =maxParallel.load();
         while(p > max) {
           maxParallel.compare_exchange_strong(max,p);
         }
-        auto n = nIterations/5/10;
+        auto n = nIterations/5/20;
         if(tid != threadID()) {
           task_on_thread_running(true);
-        } /*else {
-          n = n/10;
-          } */
+        }
         if(iIndex == 4) {
-          n *= 5;
+          n *= 3;
         }
         usleep(n);
         if(tid != threadID() ) {
@@ -175,8 +195,10 @@ namespace {
       });
 
     if(maxParallel > 1) {
+#if defined(DEBUG_OUT)
       std::unique_lock<std::mutex> l{s_coutMutex};
       std::cout <<"max parallel " <<maxParallel.load()<<std::endl;
+#endif
     }
 
     task_on_thread_running(false);
@@ -202,8 +224,8 @@ int main(int argc, char * argv[] ) {
 
   const int nEvents = std::stoi(argv[3]);
   ProcessingState state = {nEvents, 0};
-  constexpr long int nIterations = 1000000;
-  //constexpr long int nIterations = 100;
+  constexpr long int nIterations = 1000000; // 1 second in usecs
+
   MockStorage storage(nIterations);
 
   const int nStreams = std::stoi(argv[2]);
@@ -217,7 +239,7 @@ int main(int argc, char * argv[] ) {
     }
   }
 
-  std::cout <<"n Threads "<<nThreads<<" nStreams "<<nStreams <<" nEvents "<< nEvents<<std::endl;
-  std::cout <<"n processed "<<state.m_nProcessed.load()<<" max concurrent toplevel tasks "<<maxRunningTasks.load() <<std::endl;
+  std::cout <<"n Threads "<<nThreads<<" # concurrent processing loops "<<nStreams <<" # items to process "<< nEvents<<std::endl;
+  std::cout <<"# processed "<<state.m_nProcessed.load() <<" times had multiple tasks on a thread "<< nTimesMultipleTasksOnThread << " max concurrent toplevel tasks "<<maxRunningTasks.load() <<std::endl;
   return 0;
 }
